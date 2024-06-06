97af4115f2fdaa4f5675b9b1a09134a00480f3da
shireen-code <fatima.arif@buhlergroup.com>
2024-06-06 16:16:53 +0200

    updates

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
-import empoyeesRoute from './routes/employeesRoute.js';'
+import empoyeesRoute from './routes/employeesRoute.js';
 import cors from 'cors';
 
 const app = express();
